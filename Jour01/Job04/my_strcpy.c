41076edd04f6c395c1948eacbf6c3a60afdaf5a7
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2023-11-27 14:46:18 +0100

    feat(Jour01/Job04): implement concise my_strcpy function
    Implement my_strcpy function in my_strcpy.c using a more concise while loop for copying the source string into the destination string.

---
+++ Jour01/Job04/my_strcpy.c
@@ -0,0 +1,12 @@
+// File: my_strcpy.c
+// Description: Implementation of the my_strcpy function.
+
+// Function: my_strcpy
+// Parameters:
+//   - dest: a pointer to the destination null-terminated string
+//   - src: a pointer to the source null-terminated string
+// Description: Copies the source string into the destination string.
+void my_strcpy(char *dest, const char *src)
+{
+    while ((*dest++ = *src++));
+}
