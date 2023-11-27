931dc3cb281eeac028d870f6ff8d8e8562425291
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2023-11-27 16:28:43 +0100

    feat(Jour01/Job06): optimize and secure my_strcmp function
    Optimize and secure the implementation of my_strcmp function in my_strcmp.c using a more concise while loop.

---
+++ Jour01/Job06/my_strcmp.c
@@ -0,0 +1,17 @@
+// File: my_strcmp.c
+// Description: Implementation of the my_strcmp function.
+
+// Function: my_strcmp
+// Parameters:
+//   - str1: a pointer to the first null-terminated string
+//   - str2: a pointer to the second null-terminated string
+// Description: Compares two strings and returns 0 if they are identical,
+// a negative value if the first string is less than the second, or a positive
+// value if the first string is greater than the second.
+int my_strcmp(const char *str1, const char *str2)
+{
+    while (*str1 && *str1 == *str2)
+        ++str1, ++str2;
+
+    return *(unsigned char *)str1 - *(unsigned char *)str2;
+}
