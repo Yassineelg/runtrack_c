f3baf16bb2612d1f5647dae4da3b8c94fd9455de
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2023-11-27 16:02:34 +0100

    feat(Jour01/Job05): optimize my_strdup function
    Optimize the calculation of the input string length in my_strdup.c using a more concise for loop condition.

---
+++ Jour01/Job05/my_strdup.c
@@ -0,0 +1,29 @@
+// File: my_strdup.c
+// Description: Implementation of the my_strdup function.
+
+#include <stdlib.h>
+
+// Function: my_strdup
+// Parameters:
+//   - str: a pointer to a null-terminated string
+// Description: Allocates memory for a copy of the input string and copies it.
+char *my_strdup(const char *str)
+{
+    // Calculate the length of the input string.
+    size_t length;
+    for (length = 0; str[length]; ++length);
+
+    // Allocate memory for the copy.
+    char *copy = (char *)malloc((length + 1) * sizeof(char));
+
+    // Check if memory allocation is successful.
+    if (copy == NULL) {
+        return NULL; // Return NULL if malloc fails.
+    }
+
+    // Copy the string into the newly allocated memory.
+    char *dest = copy;
+    while ((*dest++ = *str++));
+
+    return copy;
+}
