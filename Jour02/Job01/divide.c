ad2c8fdce73fc7044817b682813a9b8dd774ba19
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2023-11-27 17:40:33 +0100

    feat(Jour02/Job01): implement divide function
    Implemented the divide function in divide.c, which takes a pointer to an integer and divides its value by 2. The function has been optimized for brevity and clarity.

---
+++ Jour02/Job01/divide.c
@@ -0,0 +1,12 @@
+// File: divide.c
+// Description: Implementation of the divide function.
+
+// Function: divide
+// Parameters:
+//   - val: a pointer to an integer
+// Description: Divides the value of the integer by 2.
+void divide(int *val) {
+    if (val) {
+        *val /= 2;
+    }
+}
